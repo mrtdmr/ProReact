999185f3333e6b4d2057b5b41ab680091050a4a1
mrtdmr <muratdemir.pau@gmail.com>
2019-10-31 16:39:53 +0300

    31.10.2019_Initial

---
+++ todo/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+//import logo from './logo.svg';
+//import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h4 className="bg-primary text-white text-center p-2"> To Do List</h4>
+      </div>
+    );
+  }
+}
+
+export default App;
